2390d596af8e3b713bd7b6ed1f467f4efe981993
Dmitry Kutakov <vkd.castle@gmail.com>
2020-02-24 19:05:51 +0100

    game_test - rm unused code

--- core/game_test.go
+++ core/game_test.go
@@ -11,20 +11,12 @@ func TestZeroGame(t *testing.T) {
 	game, err := NewGame(WithSeed(0))
 	require.NoError(t, err)
 
-	wonders := game.GetAvailableWonders()
-
-	// [0:The Appian Way 1:The Statue of Zeus 2:The Great Library 3:Temple of Artemis
-	// 4:The Hanging Gardens 5:The Great Lighthouse 6:The Mausoleum 7:The Sphinx]
-	assert.Len(t, wonders, initialWonders)
-
 	err = game.SelectWonders(
-		// Temple of Artemis, The Great Library, The Hanging Gardens, The Sphinx
-		[...]WonderID{wonders[3], wonders[2], wonders[4], wonders[7]},
-		// The Appian Way, The Statue of Zeus, The Great Lighthouse, The Mausoleum
-		[...]WonderID{wonders[0], wonders[1], wonders[5], wonders[6]},
+		[...]WonderID{wonderID("Temple of Artemis"), wonderID("The Great Library"), wonderID("The Hanging Gardens"), wonderID("The Sphinx")},
+		//
+		[...]WonderID{wonderID("The Appian Way"), wonderID("The Statue of Zeus"), wonderID("The Great Lighthouse"), wonderID("The Mausoleum")},
 	)
 	require.NoError(t, err)
-	assert.Equal(t, game.GetState(), StateGameTurn)
 
 	// 0
 	_, err = game.ConstructBuilding(cardID("Logging camp"))
